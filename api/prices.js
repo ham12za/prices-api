848aa31ffcb7b3a0237a79877aac2d1fffb49dba
ham12za <h.abuelnasr@hotmail.com>
2026-04-24 07:25:06 -0400

    Create prices.js

---
+++ api/prices.js
@@ -0,0 +1,17 @@
+export default async function handler(req, res) {
+  const url =
+    "https://script.google.com/macros/s/AKfycbyNHWc6BN5hvCpi7ugEWsh4K13AAAft8Lp57EgwROHb63WhpSs3pb982RpUQMwuhjrDlA/exec";
+
+  const response = await fetch(url, {
+    redirect: "follow",
+    headers: { Accept: "application/json" },
+  });
+
+  const data = await response.text();
+
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Cache-Control", "no-store");
+
+  res.status(200).send(data);
+}
