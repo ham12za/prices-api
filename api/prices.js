23d53ae90834171b0083be859c161762ee865b5d
ham12za <h.abuelnasr@hotmail.com>
2026-04-24 08:17:52 -0400

    Update prices.js

--- api/prices.js
+++ api/prices.js
@@ -1,6 +1,17 @@
+module.exports = async (req, res) => {
+  const url =
+    "https://script.google.com/macros/s/AKfycbyNHWc6BN5hvCpi7ugEWsh4K13AAAft8Lp57EgwROHb63WhpSs3pb982RpUQMwuhjrDlA/exec";
 
-export default function handler(req, res) {
-  res.status(200).json({
-    status: "working"
+  const response = await fetch(url, {
+    redirect: "follow",
+    headers: { Accept: "application/json" },
   });
-}
+
+  const data = await response.text();
+
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Cache-Control", "no-store");
+
+  res.status(200).send(data);
+};
